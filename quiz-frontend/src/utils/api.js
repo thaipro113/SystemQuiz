aebd26edfd7d814bfe4ef54269ec5a87fcce0049
LongTime <longgt10@gmai.com>
2026-05-21 16:12:03 +0700

    Sửa lỗi cổng kết nối Backend và làm động đường dẫn API cho Frontend

--- quiz-frontend/src/utils/api.js
+++ quiz-frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://localhost:44301/api',
+    baseURL: import.meta.env.VITE_API_URL || 'https://localhost:44301/api',
 });
 
 api.interceptors.request.use((config) => {
